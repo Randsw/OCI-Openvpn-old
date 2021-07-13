9befb680651beeacf076819f3cd832433938b84f
Randsw <randsw@mail.ru>
2021-07-13 16:22:50 +0300

    Create new compartment for OpenVPN. Create security list. Run deploy from non administrator user

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
 }
 
 data "sops_file" "secret" {
-  source_file = "secret.sops.yaml"
+  source_file = "secret_dep.sops.yaml"
 }
 
 provider "oci" {
@@ -24,70 +24,112 @@ provider "oci" {
 }
 
 data "oci_identity_availability_domains" "ads" {
-  compartment_id = data.sops_file.secret.data["tenancy"]
+  compartment_id = data.oci_identity_compartments.openvpn_compartments.compartments[0].id
 }
 
-
-# resource "oci_identity_compartment" "tf-compartment" {
+# resource "oci_identity_compartment" "openvpn-compartment" {
 #     # Required
 #     compartment_id = data.sops_file.secret.data["tenancy"]
 #     description = "Compartment for Terraform resources."
-#     name = "Rand-OCI-compartment"
+#     name = "OpenVPN-compartment"
+#     freeform_tags = {"App"= "OpenVPN"}
 # }
 
+data "oci_identity_compartments" "openvpn_compartments" {
+    compartment_id = data.sops_file.secret.data["tenancy"]
+    access_level = "ACCESSIBLE"
+    name = "OpenVPN"
+    state = "ACTIVE"
+}
 
 resource "oci_core_vcn" "internal" {
   dns_label      = "internal"
   cidr_block     = "172.16.0.0/20"
-  compartment_id = data.sops_file.secret.data["tenancy"]
+  compartment_id = data.oci_identity_compartments.openvpn_compartments.compartments[0].id
   display_name   = "OpenVPN VCN"
+  freeform_tags = {"App"= "OpenVPN"}
 }
 
 resource "oci_core_subnet" "openvpn" {
   vcn_id                      = oci_core_vcn.internal.id
   cidr_block                  = "172.16.0.0/24"
-  compartment_id              = data.sops_file.secret.data["tenancy"]
+  compartment_id              = data.oci_identity_compartments.openvpn_compartments.compartments[0].id
   display_name                = "OpenVPN server subnet"
-  dns_label                   = "dev"
+  dns_label                   = "openvpn"
   route_table_id              = oci_core_route_table.OpenVPN-Server.id
+  freeform_tags               = {"App"= "OpenVPN"}
+  dhcp_options_id             = oci_core_vcn.internal.default_dhcp_options_id
+  security_list_ids           = [oci_core_security_list.OpenVPN_security_list.id]
 }
 
 resource "oci_core_route_table" "OpenVPN-Server" {
 
-    compartment_id = data.sops_file.secret.data["tenancy"]
+    compartment_id = data.oci_identity_compartments.openvpn_compartments.compartments[0].id
     vcn_id = oci_core_vcn.internal.id
 
 
     display_name = "OpenVPN Route Table"
-    freeform_tags = {"Access"= "OpenVPN"}
+    freeform_tags = {"App"= "OpenVPN"}
     route_rules {
-
         network_entity_id = oci_core_internet_gateway.OpenVPN_internet_gateway.id
-
         description = "OpenVPN route rule"
         destination = "0.0.0.0/0"
         destination_type = "CIDR_BLOCK"
     }
 }
 
-
 resource "oci_core_internet_gateway" "OpenVPN_internet_gateway" {
-
-    compartment_id = data.sops_file.secret.data["tenancy"]
+    compartment_id = data.oci_identity_compartments.openvpn_compartments.compartments[0].id
     vcn_id = oci_core_vcn.internal.id
-
-
     enabled = true
     display_name = "OpenVPN IG"
-    freeform_tags = {"Access"= "OpenVPN"}
+    freeform_tags = {"App"= "OpenVPN"}
 }
 
+resource "oci_core_security_list" "OpenVPN_security_list" {
+    compartment_id = data.oci_identity_compartments.openvpn_compartments.compartments[0].id
+    vcn_id = oci_core_vcn.internal.id
+    display_name = "OpenVPN Security List"
+    freeform_tags = {"App"= "OpenVPN"}
+    egress_security_rules {
+        protocol    = "6"
+        destination = "0.0.0.0/0"
+    }
+    ingress_security_rules {
+        protocol = "6"
+        source   = "0.0.0.0/0"
+        description = "Allow ssh"
+        tcp_options {
+            max = "22"
+            min = "22"
+        }
+    }
+    ingress_security_rules {
+        protocol = 17
+        source = "0.0.0.0/0"
+        description = "OpenVPN UDP rule"
+        udp_options {
+            max = 1194
+            min = 1194
+        }
+    }
+    ingress_security_rules {
+        protocol = 6
+        source = "0.0.0.0/0"
+        description = "OpenVPN TCP rule"
+        tcp_options {
+            max = 443
+            min = 443
+        }
+    }
+}
 
 resource "oci_core_instance" "OpenVPN" {
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[2].name
-  compartment_id      = data.sops_file.secret.data["tenancy"]
+  compartment_id      = data.oci_identity_compartments.openvpn_compartments.compartments[0].id
   display_name        = "OpenVPN Server"
   shape               = "VM.Standard.E2.1.Micro"
+  freeform_tags       = {"App"= "OpenVPN"}
 
   create_vnic_details {
     subnet_id        = oci_core_subnet.openvpn.id
@@ -98,7 +140,7 @@ resource "oci_core_instance" "OpenVPN" {
 
   source_details {
     source_type = "image"
-    source_id   = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaymguk5srho2luw7w627fm3sshgtgpsfkzmeiec3qrrwsy3ys76fa"
+    source_id   = "${var.image_id[var.region]}"
   }
 
   metadata = {
@@ -109,19 +151,22 @@ resource "oci_core_instance" "OpenVPN" {
       host        = "${self.public_ip}"
       type        = "ssh"
       user        = "opc"
-      private_key = "${file("~/ya_rsa")}"
+      private_key = "${file(var.ssh_key_private)}"
     }
   provisioner "remote-exec" {
     inline = ["sudo yum check-update", "sudo yum install python3 -y", "echo Done!"]
    }
 }
 
+resource "null_resource" "ansible_provision" {
+   provisioner "local-exec" {
+     command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u opc -i '${oci_core_instance.OpenVPN.public_ip},' --private-key ${var.ssh_key_private} ansible/openvpn-server.yml"
+   }
+}
 
-# resource "null_resource" "ansible_provision" {
-#    provisioner "local-exec" {
-#      command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u opc -i '${oci_core_instance.OpenVPN.public_ip},' --private-key ${var.ssh_key_private} ansible/openvpn-server.yml"
-#    }
-# }
+output "comparement" {
+  value = data.oci_identity_compartments.openvpn_compartments.compartments[0].id
+}
 
 output "private-ip-address" {
   value = oci_core_instance.OpenVPN.private_ip
@@ -135,5 +180,3 @@ output "all-availability-domains-in-your-tenancy" {
   value = data.oci_identity_availability_domains.ads.availability_domains
 }
 
-#Ubuntu 20.04
-#ocid1.image.oc1.eu-frankfurt-1.aaaaaaaakdtauwupkvi54552qmli3ozzj5zdwlhdfzcluphhyawzv7tqeu7q
--- variables.tf
+++ variables.tf
@@ -7,3 +7,17 @@ variable "ssh_key_private" {
   default     = "~/ya_rsa"
   description = "Path to the SSH public key for accessing cloud instances. Used for creating AWS keypair."
 }
+
+variable "region" {
+  default = "eu-frankfurt-1" 
+  description = "Tenancy region"
+}
+
+variable "image_id" {
+  type = map(string)
+  default = {
+    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaymguk5srho2luw7w627fm3sshgtgpsfkzmeiec3qrrwsy3ys76fa"
+  }
+}
+#Ubuntu 20.04
+#ocid1.image.oc1.eu-frankfurt-1.aaaaaaaakdtauwupkvi54552qmli3ozzj5zdwlhdfzcluphhyawzv7tqeu7q
